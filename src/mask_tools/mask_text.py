0da88d0d6a419a666129e5e15f8bcb8d3c23ebce
大森雄基 <s1711348@coins.tsukuba.ac.jp>
2020-11-16 02:22:57 +0900

    split_into_words が名詞のみを抽出できるように変更

--- src/mask_tools/mask_text.py
+++ src/mask_tools/mask_text.py
@@ -3,11 +3,16 @@ from gensim import matutils
 from gensim.corpora import Dictionary
 
 
-def split_into_words (text):
+def split_into_words(text, only_noun=False):
     tagger = MeCab.Tagger("-Owakati")
     node = tagger.parseToNode(text)
     words = []
     while node:
+        if only_noun:
+            feature = node.feature.split(",")
+            if feature[0] != "名詞":
+                node = node.next
+                continue
         word = node.surface
         words.append(word)
         node = node.next
